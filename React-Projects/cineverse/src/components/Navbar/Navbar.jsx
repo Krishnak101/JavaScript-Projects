bfa73da66e6d5dac005d0bffa14d667b166a4fd9
Krishnak101 <kunatibala101@gmail.com>
2025-10-15 00:34:32 -0400

    added Sign Out feature

--- React-Projects/cineverse/src/components/Navbar/Navbar.jsx
+++ React-Projects/cineverse/src/components/Navbar/Navbar.jsx
@@ -3,29 +3,48 @@ import "./Navbar.css";
 import Fire from "../../assets/fire.png";
 import Star from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Logo from "./Logo";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
 // import DarkMode from "../DarkMode/DarkMode";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
   return (
     <nav className="navbar">
       <div>
         <Logo />
       </div>
 
-      <div className="navbar_links">
+      <div className="navbar_links ">
         {/* <DarkMode /> */}
-        <NavLink to="/home/popular">
+        <NavLink className="navbar_link" to="/home/popular">
           Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
         </NavLink>
-        <NavLink to="/home/top_rated">
+        <NavLink className="navbar_link" to="/home/top_rated">
           Top Rated <img src={Star} alt="star emoji" className="navbar_emoji" />
         </NavLink>
-        <NavLink to="/home/upcoming">
+        <NavLink className="navbar_link" to="/home/upcoming">
           Upcoming{" "}
           <img src={Party} alt="party emoji" className="navbar_emoji" />
         </NavLink>
+        <button
+          onClick={handleSignOut}
+          className="signout_btn bg-red-600 text-white font-bold rounded hover:bg-red-700 transition"
+        >
+          Sign Out
+        </button>
       </div>
     </nav>
   );
