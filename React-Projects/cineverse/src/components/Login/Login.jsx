a4b16f8e2a65fe45a1e262cc302d77daf241cd3e
Krishnak101 <kunatibala101@gmail.com>
2025-10-16 19:20:27 -0400

    created a seperate file for storing constants

--- React-Projects/cineverse/src/components/Login/Login.jsx
+++ React-Projects/cineverse/src/components/Login/Login.jsx
@@ -11,6 +11,7 @@ import Logo from "../Navbar/Logo";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setUserState } from "../utils/userSlice";
+import { BACKGROUND_IMAGE_URL } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -79,7 +80,7 @@ const Login = () => {
       <Logo />
       <div className="absolute">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/25f808aa-cecb-4753-8541-9a79f40c18ae/web/CA-en-20251006-TRIFECTA-perspective_6136ff47-a51d-4468-83da-8477312ee5fa_large.jpg"
+          src={BACKGROUND_IMAGE_URL}
           alt="Background Image"
           className="w-full"
         />
@@ -133,3 +134,4 @@ const Login = () => {
 };
 
 export default Login;
+ 
---
+++ React-Projects/cineverse/src/components/utils/constants.jsx
@@ -0,0 +1,2 @@
+export const BACKGROUND_IMAGE_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/25f808aa-cecb-4753-8541-9a79f40c18ae/web/CA-en-20251006-TRIFECTA-perspective_6136ff47-a51d-4468-83da-8477312ee5fa_large.jpg";
