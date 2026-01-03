5c6af219d185ce45d92fcbcc7159c59729b9837a
Krishnak101 <kunatibala101@gmail.com>
2026-01-02 21:13:58 -0500

    updated to Node ES modules using import, instead of require()

--- React-Projects/DevIdentity/server.js
+++ React-Projects/DevIdentity/server.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const connectDB = require("./config/db");
+import express from "express";
+import connectDB from "./config/db.js";
 
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send("API running "));
+app.get("/", (req, res) => res.send("API running "));
+
+//define routes
+app.use("/api/users", (await import("./routes/api/users.js")).default);
+app.use("/api/auth", (await import("./routes/api/auth.js")).default);
+app.use("/api/profile", (await import("./routes/api/profile.js")).default);
+app.use("/api/posts", (await import("./routes/api/posts.js")).default);
+
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
