d68a99febed020e0015b2dd831249f7cb96b6cf6
Krishnak101 <kunatibala101@gmail.com>
2026-01-17 18:36:18 -0500

    Pagination Feature added

--- React-Projects/DevIdentity/client/src/components/Profiles/Profiles.jsx
+++ React-Projects/DevIdentity/client/src/components/Profiles/Profiles.jsx
@@ -1,39 +1,53 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProfiles } from "../utils/actions/profile";
 import Spinner from "../Home/Spinner";
 import ProfileItem from "./ProfileItem";
 import "./Profiles.css";
+import { setAlertWithTimeOut } from "./../utils/actions/alerts";
+import Pagination from "../Routing/Pagination";
 
 const Profiles = () => {
   const dispatch = useDispatch();
   const profileStore = useSelector((state) => state.profile);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(4);
+  const endIndex = currentPage * postsPerPage;
+  const startIndex = endIndex - postsPerPage;
+  let limitedProfiles = [];
   useEffect(() => {
     dispatch(getAllProfiles());
   }, []);
+  if (!profileStore.isProfilesLoaded) {
+    return <Spinner />;
+  } else {
+    limitedProfiles = profileStore.profiles?.slice(startIndex, endIndex);
+  }
   return (
-    <Fragment>
-      {!profileStore.isProfilesLoaded ? (
-        <Spinner />
-      ) : (
-        <div className="profiles_top_container">
-          <h1 className="text_header font-bold text-2xl">Developers</h1>
-          <p className="lead">
-            <i className="fab fa-connectdevelop"></i> Browse and Connect with
-            Developers
-          </p>
-          <div>
-            {profileStore.profiles.length > 0 ? (
-              profileStore.profiles.map((profile) => (
-                <ProfileItem key={profile._id} profile={profile} />
-              ))
-            ) : (
-              <h3> No Profiles Found...</h3>
-            )}
-          </div>
-        </div>
-      )}
-    </Fragment>
+    <div className="profiles_top_container">
+      <h1 className="text_header font-bold text-2xl">Developers</h1>
+      <p className="lead">
+        <i className="fab fa-connectdevelop"></i> Browse and Connect with
+        Developers
+      </p>
+      <div>
+        {profileStore.profiles.length > 0 ? (
+          <Fragment>
+            {limitedProfiles.map((profile, index) => (
+              <ProfileItem key={profile._id} profile={profile} />
+            ))}
+            <Pagination
+              totalPosts={profileStore.profiles.length}
+              postsPerPage={postsPerPage}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </Fragment>
+        ) : (
+          <h3> No Profiles Found...</h3>
+        )}
+      </div>
+    </div>
   );
 };
 
