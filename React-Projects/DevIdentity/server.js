ac4a4c4cc64b231c42dff5f74ac0573680ebb823
Krishnak101 <kunatibala101@gmail.com>
2026-01-21 20:45:42 -0500

    Setting the build for production

--- React-Projects/DevIdentity/server.js
+++ React-Projects/DevIdentity/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 connectDB();
 const app = express();
@@ -8,8 +10,9 @@ const PORT = process.env.PORT || 5000;
 //initializing middleware for json body parsing capability
 app.use(express.json({ extended: false }));
 
-//define a simple route
-app.get("/", (req, res) => res.send("API running "));
+// ES Module fix for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //define routes
 app.use("/api/users", (await import("./routes/api/users.js")).default);
@@ -17,4 +20,11 @@ app.use("/api/auth", (await import("./routes/api/auth.js")).default);
 app.use("/api/profile", (await import("./routes/api/profile.js")).default);
 app.use("/api/posts", (await import("./routes/api/posts.js")).default);
 
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
